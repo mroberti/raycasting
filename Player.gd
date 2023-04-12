b3e03865aad2d33afddf9d541f4f8b9c2b9e5666
Mario Roberti <mario.roberti@gmail.com>
2023-04-11 17:35:09 -0700

    Got some basic mouse movement in
    Mouse move example

--- Player.gd
+++ Player.gd
@@ -1,14 +1,19 @@
 extends KinematicBody2D
 
-var speed = 100
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2(speed, 0).rotated(rotation)
 
 func _physics_process(delta):
-	var vel = Vector2(
-		int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left")),
-		int(Input.is_action_pressed("ui_down"))-int(Input.is_action_pressed("ui_up"))
-	)
-	
-	if Input.action_press("ui_select") and $RayCast2D.is_colliding():
-		print("object recognized")
-	
-	move_and_slide(vel*speed)
+	get_input()
+	velocity = move_and_slide(velocity)
+
+# Form https://docs.godotengine.org/en/3.2/tutorials/2d/2d_movement.html
