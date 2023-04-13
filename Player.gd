d628592ec6d7c5c87ed3c671af24e2339625fa45
Mario Roberti <mario.roberti@gmail.com>
2023-04-12 17:28:53 -0700

    Added example from Node Player
    Added 2D player object. Hope it works.

--- Player.gd
+++ Player.gd
@@ -1,19 +1,30 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
+export (float) var rotation_speed = 1.5
 
 var velocity = Vector2()
+var rotation_dir = 0
 
 func get_input():
-	look_at(get_global_mouse_position())
+	rotation_dir = 0
 	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		rotation_dir += 1
+	if Input.is_action_pressed("ui_left"):
+		rotation_dir -= 1
 	if Input.is_action_pressed("ui_down"):
 		velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed("ui_up"):
 		velocity = Vector2(speed, 0).rotated(rotation)
 
 func _physics_process(delta):
+	if $RayCast2D.is_colliding():
+		print("Colliding!",Time.get_time_string_from_system())
+		print($RayCast2D.get_collider().name)
+
 	get_input()
+	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity)
 
-# Form https://docs.godotengine.org/en/3.2/tutorials/2d/2d_movement.html
+# Form https://docs.godotengine.org/en/3.5/tutorials/2d/2d_movement.html
