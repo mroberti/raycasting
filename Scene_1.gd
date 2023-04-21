be7c0d92b19ceaa861c3f1e19e76c53c4d0c6d8d
Mario Roberti <mario.roberti@gmail.com>
2023-04-20 17:28:38 -0700

    dumb
    dumb

--- Scene_1.gd
+++ Scene_1.gd
@@ -5,13 +5,22 @@ extends Node2D
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass;
+	set_process_input(true) 
+	##This may or may not be required for inputs to work.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass;
+func _input(event):
+	if event is InputEventKey and event.is_action_released("KEY_J"):
+		if event.scancode != KEY_ENTER:
+			print(event.scancode)
+			match event.scancode:
+				"KEY_Z":
+					print("1")
+				"KEY_Y":
+					print("2")
+
+			#Do what you gotta do.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
